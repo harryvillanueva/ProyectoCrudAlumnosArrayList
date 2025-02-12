883d8b52fcd68a13778c2f7e6d18eec52f30899b
harryvillanueva <harryvillanueva0814@gmail.com>
2025-02-12 01:56:17 +0100

    subir funcionalidad listar alumnos

--- src/alumno/Alumno.java
+++ src/alumno/Alumno.java
@@ -26,16 +26,44 @@ public class Alumno {
     }
 
 
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getFechaNac() {
+        return fechaNac;
+    }
+
+    public String getAsignatura1() {
+        return asignatura1;
+    }
+
+    public String getAsignatura2() {
+        return asignatura2;
+    }
+
+    public double getNostaAsignatura1() {
+        return nostaAsignatura1;
+    }
+
+    public double getNotaAsignatura2() {
+        return notaAsignatura2;
+    }
+
     @Override
     public String toString() {
         return
-                "nombreCompleto= " + nombreCompleto + " "+
-                " edad=" + edad + " "+
-                " fechaNac='" + fechaNac + " " +
-                " asignatura1='" + asignatura1 + " " +
-                " nostaAsignatura1=" + nostaAsignatura1 + " "+
-                " asignatura2='" + asignatura2 + " " +
-                " notaAsignatura2=" + notaAsignatura2
+                 nombreCompleto + " "+
+                 edad + " "+
+                 fechaNac + " " +
+                 asignatura1 + " " +
+                 nostaAsignatura1 + " "+
+                 asignatura2 + " " +
+                notaAsignatura2
                 ;
     }
 }
--- src/output/Output.java
+++ src/output/Output.java
@@ -1,5 +1,7 @@
 package output;
 
+import alumno.Alumno;
+
 public class Output {
 
     public void mostrarMenu(){
@@ -37,6 +39,26 @@ public class Output {
     public void mostrarMensajeNota(){
 
         System.out.println("Ingrese nota de asignatura");
+    }
+
+    public void listarAlumnos(Alumno alumno){
+
+        System.out.println(" "+ alumno);
+    }
+
+    public void imprimirCabecera(String formato){
+
+        System.out.printf(formato, "Nombre Alumno", "Edad", "Fecha", "Asignatura 1", "Nota", "Asignatura 2", "Nota");
+    }
+
+    public void imprimirAlumno(String formato, String nombre, int edad, String fecha, String asignatura1, double nota1,String asignatura2 , double nota2){
+
+        System.out.printf(formato, nombre, edad, fecha, asignatura1, nota1, asignatura2, nota2);
+    }
+
+    public void imprimirLinea(){
+        System.out.println("+-----------------+------+--------------+-----------------+------+-----------------+------+");
+
     }
 
 
